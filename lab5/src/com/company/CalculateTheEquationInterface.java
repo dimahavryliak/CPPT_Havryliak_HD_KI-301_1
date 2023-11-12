91d7c7d748a05dad3666a9dd03eaf28436cdc2b5
dimahavryliak <127104103+dimahavryliak@users.noreply.github.com>
2023-11-12 23:06:08 +0200

    Add files via upload

---
+++ lab5/src/com/company/CalculateTheEquationInterface.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface CalculateTheEquationInterface {
+    double doCalculation();
+}
---
+++ lab5/src/com/company/ReadWriteData.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface ReadWriteData {
+    void writeResultToTxt(String fileName) throws IOException;
+    void writeResultToBin(String fileName) throws IOException;
+
+    void readResultFromTxt(String fileName) throws IOException;
+    void readResultFromBin(String fileName) throws IOException;
+}
